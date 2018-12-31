83b786dafee65cb896d6a7ef76efe25c2845f4bd
Ozan Karaali <ozan.karaali@gmail.com>
2019-01-01 00:02:42 +0300

    push ints

--- parser-pest/src/main.rs
+++ parser-pest/src/main.rs
@@ -4,7 +4,11 @@ extern crate pest_derive;
 use std::fs;
 use common::bytecode::Inst;
 
-use pest::Parser;
+use pest::{Parser,
+    //error::Error as PestError,
+    //iterators::{Pair, Pairs},
+    //prec_climber::{Operator, PrecClimber, Assoc}
+};
 
 #[derive(Parser)]
 #[grammar = "grammar.pest"]
@@ -20,13 +24,63 @@ fn main() {
     let mut insts: Vec<Inst> = vec![];
     for line in file.into_inner() {
         match line.as_rule() {
-            Rule::letalloc => {
+            Rule::let_allocate => {
                 let mut inner_rules = line.into_inner(); // { name ~ "=" ~ value }
 
                 let result: &str = inner_rules.next().unwrap().as_str();
                 insts.push(Inst::Alloc{
                     name: result.to_string()
                 })
+            }
+            Rule::function_call=>{
+                let mut inner_rules = line.into_inner(); // { name ~ "=" ~ value }
+
+                let func: &str = inner_rules.next().unwrap().as_str();
+                let args: &str = inner_rules.next().unwrap().as_str();
+                insts.push(Inst::Call{
+                    name: func.to_string(),
+                    arguments: vec![args.to_string()].into(),
+                    this: None
+                })
+            }
+            Rule::assignment=>{
+                let mut inner_rules = line.into_inner(); // { name ~ "=" ~ value }
+                let variable = inner_rules.next().unwrap();
+                let mut var_name;
+                match variable.as_rule() {
+                    Rule::let_allocate => {
+                        let mut inner_rules = variable.into_inner(); // { name ~ "=" ~ value }
+                        let result: &str = inner_rules.next().unwrap().as_str();
+                        insts.push(Inst::Alloc{
+                            name: result.to_string()
+                        });
+                        var_name = result.to_string();
+                        },
+                    Rule::assignment => {
+                        let mut inner_rules = variable.into_inner(); // { name ~ "=" ~ value }
+                        var_name = inner_rules.next().unwrap().as_str().to_string();
+                        },
+                    _ => unreachable!()
+                }
+                //println!("{:?}",variable);
+                let args = inner_rules.next().unwrap();
+                match args.as_rule() {
+                    Rule::integer => {
+                        let result = args.as_str().parse::<i64>().unwrap(); // { name ~ "=" ~ value }
+                        insts.push(Inst::PushInt{
+                            name: var_name,
+                            value: result
+                        });
+                        },
+                    Rule::float => {
+                        let result = args.as_str().parse::<f64>().unwrap(); // { name ~ "=" ~ value }
+                        insts.push(Inst::PushFloat{
+                            name: var_name,
+                            value: result
+                        });
+                    }
+                    _ => {println!("{:?}",args)}
+                }
             }
             Rule::EOI => (),
             _ => unreachable!(),

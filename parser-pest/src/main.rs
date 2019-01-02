594a21da9f8acdcda067e45e2e8027027a13dc97
Ozan Karaali <ozan.karaali@gmail.com>
2019-01-02 22:55:05 +0300

    parser expression test

--- parser-pest/src/main.rs
+++ parser-pest/src/main.rs
@@ -9,16 +9,51 @@ extern crate engine;
 use common::bytecode::Inst;
 use common::{new_syncmut, SyncMut};
 use engine::{ExecutionContext, ExecutionEngine};
-
 use pest::{Parser,
-    //error::Error as PestError,
-    //iterators::{Pair, Pairs},
-    //prec_climber::{Operator, PrecClimber, Assoc}
+    iterators::{Pair, Pairs},
+    prec_climber::{Operator, PrecClimber, Assoc}
 };
 
 #[derive(Parser)]
 #[grammar = "grammar.pest"]
 pub struct YumakParser;
+
+
+#[macro_use]
+extern crate lazy_static;
+lazy_static! {
+    static ref PREC_CLIMBER: PrecClimber<Rule> = {
+        PrecClimber::new(vec![
+            Operator::new(Rule::add, Assoc::Left) | Operator::new(Rule::subtract, Assoc::Left),
+            Operator::new(Rule::multiply, Assoc::Left) | Operator::new(Rule::divide, Assoc::Left) | Operator::new(Rule::modulus, Assoc::Left),
+            Operator::new(Rule::power, Assoc::Right)
+        ])
+    };
+}
+
+
+fn consume<'i>(pair: Pair<'i, Rule>, climber: &PrecClimber<Rule>) -> i32 {
+    let primary = |pair| consume(pair, climber);
+    let infix = |lhs: i32, op: Pair<Rule>, rhs: i32| match op.as_rule() {
+        Rule::add => lhs + rhs,
+        Rule::subtract => lhs - rhs,
+        Rule::multiply => lhs * rhs,
+        Rule::divide   => lhs / rhs,
+        Rule::modulus  => lhs % rhs,
+            //Rule::power    => lhs.powf(rhs),
+        Rule::power => lhs.pow(rhs as u32),
+        _ => unreachable!(),
+    };
+    //println!("{:?}",pair);
+    match pair.as_rule() {
+        Rule::expr => climber.climb(pair.into_inner(), primary, infix),
+        //Rule::number => pair.as_rule()//as_str().parse::<i32>().unwrap(),
+        Rule::integer => pair.as_str().parse::<i32>().unwrap(),
+        Rule::float => pair.as_str().parse::<i32>().unwrap(),
+        _ => 0,
+    }
+}
+
 
 fn main() {
     let unparsed_file = fs::read_to_string("./src/asd.yumak").expect("cannot read file");
@@ -69,8 +104,13 @@ fn main() {
                     _ => unreachable!()
                 }
                 //println!("{:?}",variable);
+                //let expr = 
                 let args = inner_rules.next().unwrap();
                 match args.as_rule() {
+                    Rule::expr => {
+                        //let expr = args.inner_rules().next().unwrap();
+                        println!("{:?}",consume(args, &PREC_CLIMBER));
+                    }
                     Rule::integer => {
                         let result = args.as_str().parse::<i64>().unwrap(); // { name ~ "=" ~ value }
                         insts.push(Inst::PushInt{

19ea3ec40c81a6942b80edcf8d7c803e615f137a
Ozan Karaali <ozan.karaali@gmail.com>
2020-12-24 22:48:48 +0300

    tuple?

--- parser/src/parser.rs
+++ parser/src/parser.rs
@@ -31,40 +31,41 @@ fn build_ast_from_values(pair: Pair<Rule>) -> Inst {
         Rule::assignment => {
             let mut pair = pair.into_inner();
             let func = pair.next().unwrap();
-            let args = pair.next();
-
+            let args = pair.next().unwrap();
             let mut var_name = "".to_string();
-            println!{"{:?}", func};
-            println!{"{:?}", args};
-            match func.as_rule() {
-            //     Rule::let_allocate => {
-            //         let mut pair = var.into_inner();
-            //         let op = pair.next().unwrap();
-            //         var_name = op.as_str().to_string(); //pair.as_span().as_str()[4..].to_string();
-            //         build_ast_from_letallocate(op)
-            //     },
-            //     Rule::variable => {
-            //         let mut pair = var.into_inner();
-            //         let op = pair.next().unwrap();
-            //         var_name = op.as_str().to_string();
-            //         // NOOP
-            //         Inst::Label{
-            //             name: op.as_str().to_string(),
-            //         }
-            //     },
+            println!{"func, {:?}", func};
+            println!{ };
+            println!{"args, {:?}", args};
+            match (func.as_rule(), args.as_rule()) {
+                (Rule::let_allocate, _) => {
+                    println!{"letalloc"};
+                    let mut pair = func.into_inner();
+                    let op = pair.next().unwrap();
+                    var_name = op.as_str().to_string();
+                    build_ast_from_letallocate(op)
+                },
+                (Rule::variable, _) => {
+                    println!{"variable"};
+                    let mut pair = func.into_inner();
+                    let op = pair.next().unwrap();
+                    var_name = op.as_str().to_string();
+                    // NOOP
+                    Inst::Label{
+                        name: op.as_str().to_string(),
+                    }
+                },
+                // It won't trigger that part.
+                (_, Rule::factor) => {
+                    println!{"infac, {:?}", pair};
+                    let mut pair = args.into_inner();
+                    let op = pair.next().unwrap();
+                    Inst::PushInt{
+                        name: var_name,
+                        value: op.as_str().parse::<i64>().unwrap()
+                    }
+                },
                 _ => unreachable!()
             }
-            // match pair.as_rule() {
-            //     Rule::integer => {
-            //         let mut pair = pair.into_inner();
-            //         let op = pair.next().unwrap();
-            //         println!{"{:?}", pair};
-            //         Inst::PushInt{
-            //             name: var_name,
-            //             value: op.as_str().parse::<i64>().unwrap()
-            //         }
-            //     },
-            // }
         },
         Rule::EOI => (
             Inst::Call{

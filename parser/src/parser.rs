b412b4a86311b61566a8505d5e71fd785d6ccabe
Ozan Karaali <ozan.karaali@gmail.com>
2020-12-24 21:47:56 +0300

    func/args?

--- parser/src/parser.rs
+++ parser/src/parser.rs
@@ -1,4 +1,4 @@
-use pest::{self, Parser, iterators::{Pair, Pairs}};
+use pest::{self, Parser, iterators::{Pair}};
 
 extern crate common;
 use common::bytecode::Inst;
@@ -30,31 +30,40 @@ fn build_ast_from_values(pair: Pair<Rule>) -> Inst {
         },
         Rule::assignment => {
             let mut pair = pair.into_inner();
-            let pair = pair.next().unwrap();
+            let func = pair.next().unwrap();
+            let args = pair.next();
+
             let mut var_name = "".to_string();
-            match pair.as_rule() {
-                Rule::let_allocate => {
-                    let mut pair = pair.into_inner();
-                    let op = pair.next().unwrap();
-                    var_name = op.as_str().to_string(); //pair.as_span().as_str()[4..].to_string();
-                    println!{"{:?}", var_name}
-                    build_ast_from_letallocate(op)
-                },
-                // Rule::variable => {
-                //     // let mut pair = variable.into_inner();
-                //     var_name = variable.as_span().as_str().to_string();
-                //     },
-                _ => unreachable!()
-            }
-            // let args = pair.next().unwrap();
-            // match args.as_rule(){
-            //     Rule::factor => {
-            //         Inst::PushInt{
-            //             name: var_name,
-            //             value: args.as_str().parse::<i64>().unwrap()
+            println!{"{:?}", func};
+            println!{"{:?}", args};
+            match func.as_rule() {
+            //     Rule::let_allocate => {
+            //         let mut pair = var.into_inner();
+            //         let op = pair.next().unwrap();
+            //         var_name = op.as_str().to_string(); //pair.as_span().as_str()[4..].to_string();
+            //         build_ast_from_letallocate(op)
+            //     },
+            //     Rule::variable => {
+            //         let mut pair = var.into_inner();
+            //         let op = pair.next().unwrap();
+            //         var_name = op.as_str().to_string();
+            //         // NOOP
+            //         Inst::Label{
+            //             name: op.as_str().to_string(),
             //         }
             //     },
-            //     _ => unreachable!()
+                _ => unreachable!()
+            }
+            // match pair.as_rule() {
+            //     Rule::integer => {
+            //         let mut pair = pair.into_inner();
+            //         let op = pair.next().unwrap();
+            //         println!{"{:?}", pair};
+            //         Inst::PushInt{
+            //             name: var_name,
+            //             value: op.as_str().parse::<i64>().unwrap()
+            //         }
+            //     },
             // }
         },
         Rule::EOI => (
